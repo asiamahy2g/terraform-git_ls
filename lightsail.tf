0fffa4740ad07f8f6b10026ff9f4df535f2c8cca
Emmanuel <asiamahy2g@gmail.com>
2023-09-22 10:06:27 -0500

    new lightsail code

--- lightsail.tf
+++ lightsail.tf
@@ -1,8 +1,9 @@
 resource "aws_lightsail_instance" "light_sail_1" {
   name              = "Asiamah"
   availability_zone = "us-east-1a"
-  blueprint_id      = "centos_7_1901_01"
+  blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_1_0"
+
   user_data         = <<-EOF
     #!/bin/bash
     sudo yum update -y 
--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
-output "ip-address" {
+
+output "lightsail_instance_ip" {
   value = aws_lightsail_instance.light_sail_1.public_ip_address
 }
